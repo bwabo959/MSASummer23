58280e0617600487b3328599f9ebf934605766e1
bwabo959 <94566263+bwabo959@users.noreply.github.com>
2023-06-15 14:39:43 -0500

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,10 @@
+#declare a list
+empty_list = []
+data_list = [5, 2, 9, 1, 14, 7]
+
+#Access data in list
+print(data_list[1])
+print(data_list[4])
+
+for i in range(0, len(data_list)):
+    print(data_list[i])

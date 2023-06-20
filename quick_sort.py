a1871edb1f84eb354d1e2564049b227c9d239da3
bwabo959 <94566263+bwabo959@users.noreply.github.com>
2023-06-20 14:45:55 -0500

    Add files via upload

--- quick_sort.py
+++ quick_sort.py
@@ -1,19 +1,52 @@
-#Quicksort Algorithm
-
-def quicksort(arr):
-    
-    #if array has one item. return list because it is already sorted
-    if len(arr) <= 1:
-        return arr
-    
+"""
+Function to perform quick sort oon array
+Input; list
+Output: sorted list
+"""
+def quick_sort(arr):
+    #create a stack to simulate recursive calls
     stack = []
-    stack.append((0, len(arr)-1))
+    #Place list partitions on the stack
+    stack.append((0, len(arr) - 1))
+    #Inside While loop. Loop runs until stack of partitions is empty
+    while stack:
+        #Get the next partition to process
+        low, high = stack.pop()
+        #Partition the array. Find the pivot index for that partition. Call the partition function
+        pivot_index = partition(arr, low, high)
+        #if there are items on the left of the pivot, put them on the side
+        if pivot_index- 1 > low:
+            stack.append((low, pivot_index -1))
+        #if there are items on the right of the pivot, put them on the stack
+        if pivot_index + 1 < high:
+            stack.append((pivot_index + 1, high))
 
-    while len(stack) > 0:
-        pass
+"""
+Function to find partition index in a list
+Inputs: (list)arr, (int)low, (int)high
+Output: (int)partition index
+"""
+def partition(arr, low, high):
+    #Choose the right-most item as the pivot
+    pivot = arr[high]
+    #create a variable for the border
+    i = low - 1
+    #loop through the partition to place all items <= the pivot to the left. And >= pivot to the right
+    for j in range(low, high):
+        #if the current item is less than or equal too the pivot, swap it with the element at the border
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    #place pivot in correct position
+    #swap the pivot with the item at the border
+    arr[i+1], arr[j] = arr[high], arr[i+1]
+
+    return i + 1
 
 def main():
-    arr = [50, 30, 80, 20, 10, 70, 60]
-    quicksort(arr)
+    #create a list
+    arr = [40, 80 , 10, 90, 30, 50, 70]
+    quick_sort(arr)
+    print(arr)
 
 main()
